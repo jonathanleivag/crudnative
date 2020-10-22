be8246ea41bdf959f7cfb2811b8520a1174ebb4a
Jonathanleivag <email@jonathanleivag.com>
2020-10-21 23:55:16 -0300

    maqueta

---
+++ src/CrudApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const CrudApp = () => {
+  return (
+    <View>
+      <Text>CrudApp.js</Text>
+    </View>
+  );
+};
---
+++ src/routers/RouterApp.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const RouterApp = () => {
+  return (
+    <View>
+      <Text>RouterApp.js</Text>
+    </View>
+  );
+};
