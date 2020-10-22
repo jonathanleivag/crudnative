11b5c87da99722762f8cf9b3f945026728b6cd32
Jonathanleivag <email@jonathanleivag.com>
2020-10-22 01:10:08 -0300

    Views

---
+++ src/Views/NuevoCliente.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const NuevoCliente = () => {
+  return (
+    <View>
+      <Text>NuevoCliente.js</Text>
+    </View>
+  );
+};
--- src/routers/RouterApp.js
+++ src/routers/RouterApp.js
@@ -1,7 +1,11 @@
 import React, { Fragment } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+
+// Views
 import { InicioScreen } from "../Views/InicioScreen";
+import { NuevoCliente } from "../Views/NuevoCliente";
+import { DetalleCliente } from "../Views/DetalleCliente";
 
 const Stack = createStackNavigator();
 
@@ -11,6 +15,8 @@ export const RouterApp = () => {
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Inicial">
           <Stack.Screen name="Inicio" component={InicioScreen} />
+          <Stack.Screen name="NuevoCliente" component={NuevoCliente} options={{title:"Nuevo cliente"}}/>
+          <Stack.Screen name="DetalleCliente" component={DetalleCliente} options={{title:"Detalle cliente"}}/>
         </Stack.Navigator>
       </NavigationContainer>
     </Fragment>
