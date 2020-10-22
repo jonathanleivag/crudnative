6163f3a13fe0b1ac5e484b74727d466f2cb5fe8f
Jonathanleivag <email@jonathanleivag.com>
2020-10-22 01:43:13 -0300

    Install react-native-paper

--- src/CrudApp.js
+++ src/CrudApp.js
@@ -1,11 +1,20 @@
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import React, { Fragment } from "react";
 import { RouterApp } from "./routers/RouterApp";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+};
+
+console.log(theme);
 
 export const CrudApp = () => {
   return (
     <Fragment>
-      <RouterApp />
+      <PaperProvider theme={theme}>
+        <RouterApp />
+      </PaperProvider>
     </Fragment>
   );
 };
